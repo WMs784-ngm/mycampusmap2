b0583eb5cebfea7d228ec13f0a562c6dd1b53949
WMs784 <64195816+WMs784@users.noreply.github.com>
2021-09-16 16:03:36 +0900

    スナップショット

--- lib/main.dart
+++ lib/main.dart
@@ -7,36 +7,39 @@ import 'package:appbar_textfield/appbar_textfield.dart';
 //import 'search_building.dart';
 
 void main() => runApp(MyApp());
-
-List<double> lat = [35.6598812,35.6606455];//駒場図書館,10号館
-List<double> long = [139.6865876,139.6849458];
+//var lat = new List.generate(18, (i)=>35.6598812);//駒場図書館
+//var long = new List.generate(18,(i)=>139.6865876);
+List<double> lat = [35.6598397,35.6593117,35.6598812,35.6606455];
+List<double> long = [139.6848356,139.6836729,139.6865876,139.6849458];
 List<int> min = [101,21,101,011,511,1,721,110,900,101,1101,1211,1311,1];//教室番号の最小値
 List<int> max = [192,49,502,214,534,4,762,422,900,405,1109,1233,1341,3];//教室番号の最大値
 List<String> name = ["1号館","情報教育棟","21KOMCEE West","21KOMCEE East","5号館",
-  "コミニケーションプラザ(北)","7号館","8号館","講堂","10号館","11号館","12号館","13号館"];
-String e = "正しい教室番号を入力してください";
-String cn = e;
+  "コミニケーションプラザ(北)","7号館","8号館","講堂","10号館","11号館","12号館","13号館",
+  "アドミニストレーション棟","第一体育館","","17号館","18号館"];
+String ec = "正しい教室番号を入力してください";
+String cn = ec;
+int e = 100;
 
-String search(cn){
+int search(cn){
   if(cn.length<4){
     if(cn[0] == 'E'){
       try{//文字列から数値への変換を試す
         int i = int.parse(cn.substring(1));
-        if(min[1]<= i && i <= max[1])return "情報教育棟";
+        if(min[1]<= i && i <= max[1])return 1;
         else return e;
       }catch(exception){//変換不可能な場合例外としてここに入る
         return e;
       }
     }
-    else if(cn == "実験室")return name[3];
-    else if(cn == "剣道場")return "第一体育館";
+    else if(cn == "実験室")return 3;
+    else if(cn == "剣道場")return 14;
     try {
       int i = int.parse(cn[0]);
       if(i == 2 || i == 3 || i == 4 || i == 6 || i == 8 || i == 0)return e;
       else{
         try{
           int j = int.parse(cn);
-          if(min[i-1] <= j && j <= max[i-1])return name[i-1];
+          if(min[i-1] <= j && j <= max[i-1])return i-1;
           else return e;
         }catch(exception){
           return e;
@@ -46,26 +49,26 @@ String search(cn){
       return e;
     }
   }
-  else if(cn == "KALS")return "17号館";
+  else if(cn == "KALS")return 16;
   else if(cn[0] == "K"){
     try {
       int n = int.parse(cn.substring(2, 3));
       if (n == 0)
-        return name[2];
-      else if (n == 1) return name[3];
+        return 2;
+      else if (n == 1) return 3;
       else return e;
     }catch(exception){
       return e;
     }
   }
-  else if(cn.substring(0,3) == "実験室")return name[3];
+  else if(cn.substring(0,3) == "実験室")return 3;
   else if(cn.length > 4){
-    if(cn == "18号館ホール")return "18号館";
-    else if(cn.substring(2,5) == "実験室")return name[3];
+    if(cn == "18号館ホール")return 17;
+    else if(cn.substring(2,5) == "実験室")return 3;
     else if(cn.substring(0,2) == '8-') {
       try {
         int i = int.parse(cn.substring(2));
-        if(min[7] <= i && i <= max[7])return name[7];
+        if(min[7] <= i && i <= max[7])return 7;
         else return e;
       }catch(exception) {
         return e;
@@ -75,17 +78,17 @@ String search(cn){
       int n = int.parse(cn.substring(0,2));
       if(n == 10){
         int i = int.parse(cn.substring(3));
-        if(min[n-1] <= i && i <= max[n-1])return name[n-1];
+        if(min[n-1] <= i && i <= max[n-1])return n-1;
         else return e;
       }
       else return e;
     }catch(exception){
-      if(cn == "音楽練習室" || cn == "舞台芸術実習室") return name[5];
+      if(cn == "音楽練習室" || cn == "舞台芸術実習室") return 5;
       else if(cn.substring(0,5) == "多目的教室") {
         try {
           int i = int.parse(cn.substring(5));
           if (min[5] <= i && i <= max[5])
-            return name[5];
+            return 5;
           else
             return e;
         } catch (exception) {
@@ -95,13 +98,13 @@ String search(cn){
       else if(cn.substring(0,7) == "身体運動実習室"){
         try{
           int i = int.parse(cn.substring(7));
-          if(min[13] <= i && i <= max[13])return name[5];
+          if(min[13] <= i && i <= max[13])return 5;
           else return e;
         }catch(exception){
           return e;
         }
       }
-      else if(cn == "学際交流ホール")return "アドミニストレーション棟";
+      else if(cn == "学際交流ホール")return 13;
       else return e;
     }
   }
@@ -112,7 +115,7 @@ String search(cn){
         try {
           int i = int.parse(cn);
           if (min[d - 1] <= i && i <= max[d - 1])
-            return name[d - 1];
+            return d - 1;
           else
             return e;
         } catch (exception) {
@@ -126,12 +129,16 @@ String search(cn){
   }
 }
 double dest_lat(text){
-  if(search(text) == e)return lat[0];
-  else return lat[1];
+  if(search(text) >= lat.length)return lat[3];
+  else return lat[search(text)];
 }
 double dest_long(text){
-  if(search(text) == e)return long[0];
-  else return long[1];
+  if(search(text) >= long.length)return long[3];
+  else return long[search(text)];
+}
+String mark_name(text){
+  if(search(text) >= long.length)return ec;
+  else return name[search(text)];
 }
 
 class MyApp extends StatelessWidget {
@@ -139,7 +146,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Polyline example',
+      title: 'Komaba map',
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
@@ -196,14 +203,14 @@ class _MapScreenState extends State<MapScreen> {
           title:Text("教室番号"),
           onChanged:(text){
             cn = text;
-            //_addMarker(LatLng(dest_lat(cn), dest_long(cn)), "destination", BitmapDescriptor.defaultMarkerWithHue(90));
-            //_getPolyline();
-            _MapScreenState();
+            _addMarker(LatLng(dest_lat(cn), dest_long(cn)), "destination", BitmapDescriptor.defaultMarkerWithHue(90));
+            _getPolyline();
+            //_MapScreenState();
           }
         ),
           body: GoogleMap(
             initialCameraPosition: CameraPosition(
-                target: LatLng(_originLatitude, _originLongitude), zoom: 16),
+                target: LatLng(_originLatitude, _originLongitude), zoom: 17),
             myLocationEnabled: true,
             tiltGesturesEnabled: true,
             compassEnabled: true,
@@ -222,7 +229,9 @@ class _MapScreenState extends State<MapScreen> {
   _addMarker(LatLng position, String id, BitmapDescriptor descriptor) {
     MarkerId markerId = MarkerId(id);
     Marker marker =
-    Marker(markerId: markerId, icon: descriptor, position: position);
+    Marker(
+        markerId: markerId, icon: descriptor, position: position,infoWindow: InfoWindow(title: mark_name(cn))
+    );
     markers[markerId] = marker;
   }
 
