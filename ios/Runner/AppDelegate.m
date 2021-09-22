287d2f1b93ded09bdaedaafd3e7eab4f7d0d4c6a
WMs784 <64195816+WMs784@users.noreply.github.com>
2021-09-22 11:16:40 +0900

    APIKeyの隠蔽

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,11 +1,13 @@
 #import "AppDelegate.h"
 #import "GeneratedPluginRegistrant.h"
 #import "GoogleMaps/GoogleMaps.h"
+#import "apikey.h"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    [GMSServices provideAPIKey:@"AIzaSyDSEFIrVbRPeaFm2W_585Sn07nTWqciPho"];
+    [GMSServices provideAPIKey:maps_api_key];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
